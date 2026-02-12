747df30d88b26a62cb30ad83461997f0dedfcd07
cheeserust <jaewi96@gmail.com>
2026-02-13 08:57:12 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 432 ms, Memory: 8928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b){
+    int num1 = *(int*)a;
+    int num2 = *(int*)b;
+    if (num1 < num2) return -1;
+    if (num1 > num2) return 1;
+    return 0;
+}
+
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    int* arr = malloc(sizeof(int) * n);
+
+    for (i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, n, sizeof(int), compare);
+    for (i = 0; i < n; i++){
+        printf("%d\n", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
