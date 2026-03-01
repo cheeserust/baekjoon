3351f913aa0489de4a188d7222314992af0b39ac
cheeserust <jaewi96@gmail.com>
2026-03-01 19:28:26 +0900

    Enhance comments for clarity in 덩치.c
    Added comments to clarify the logic for ranking based on size comparisons.

--- 백준/Silver/7568. 덩치/덩치.c
+++ 백준/Silver/7568. 덩치/덩치.c
@@ -19,11 +19,15 @@ int main(){
     for (int i = 0; i < n; i++){
         int count = 0;
         for (int j = 0; j < n; j++){
+            // input[i] 를 기준으로 input[j] (즉 다른 사람들) 하고 비교해서 다른사람이 더 크면 count++ (순위가 낮아짐)
             if (input[i].x < input[j].x && input[i].y < input[j].y) count++;
         }
+        // 1위를 기준으로 가장 높은 count는 0 (자기 자신보다 덩치가 클순 없으므로)
+        // 그래서 +1 해줌
         printf("%d ", count+1);
     }
     free(input);
     return 0;
 }
+
 
