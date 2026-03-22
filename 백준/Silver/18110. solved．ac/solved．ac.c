71ffcb2ed51b27a4a2e80752704391c99e87c642
cheeserust <jaewi96@gmail.com>
2026-03-22 18:57:27 +0900

    [Silver IV] Title: solved.ac, Time: 32 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.c
+++ 백준/Silver/18110. solved．ac/solved．ac.c
@@ -1,51 +1,3 @@
-// counting sort method
-// reconstructing the sorted array ver is at the bottom
-
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    int n, i, data;
-    scanf("%d", &n);
-    //edge case handling
-    if (n == 0) {
-        printf("0\n");
-        return 0;
-    }
-    int arr[31] = {0,};  // frequence array
-
-    for (i = 0; i < n; i++){
-        scanf("%d", &data);
-        arr[data]++;
-    }
-
-    // 30% truncation
-    int low15 = (int)round(n * 0.15);
-    int high15 = (int)round(n * 0.85);
-
-    
-    int count = 0, sum = 0, sumCount = 0;
-    // count for if condition
-    // sum = total sum of truncated data
-    // sumCount = for average calculation
-    for (i = 1; i <= 30; i++){
-        for (int j = 0; j < arr[i]; j++){
-            count++;
-            if (count > low15 && count <= (n - low15)){
-                sum += i;
-                sumCount++;
-            }
-        }
-    }
-
-    int average = (int)round((double)sum / sumCount);
-    printf("%d\n", average);
-
-    return 0;
-
-}
-// reconstruct ver.
-/*
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -86,4 +38,3 @@ int main() {
     free(sorted);
     return 0;
 }
-*/
